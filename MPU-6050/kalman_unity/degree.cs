1d19b47d1f935f66a530dd27a5a4e5c351c9f1ff
jinwook <jinwook31@naver.com>
2015-08-25 17:15:33 +0900

    Serial communication code

--- MPU-6050/kalman_unity/degree.cs
+++ MPU-6050/kalman_unity/degree.cs
@@ -4,8 +4,8 @@ using System.IO.Ports;
 
 public class degree : MonoBehaviour {
 	SerialPort sp = new SerialPort("COM6", 9600);  //set Serial port
-	int[] val = new int[5] ;
-  int[] angle = new int[2] ;
+	int[] val = new int[10] ;
+    int[] angle = new int[2] ;
   
   // Use this for initialization
 	void Start() {
@@ -20,7 +20,7 @@ public class degree : MonoBehaviour {
         sp.Write("s") ;  //send start data
         val[0] = sp.ReadByte() ;  //read a byte
         if (val[0] == 0xff) {  //check start byte
-          for (int i = 1; i < 5; i++) {
+          for (int i = 1; i < 10; i++) {
             val[i] = sp.ReadByte() ;
           }
           
@@ -28,8 +28,15 @@ public class degree : MonoBehaviour {
           angle[1] = val[3] * (val[4] - 2) ;
           
           transform.rotation = Quaternion.Euler(angle[1], 0f, angle[0]) ;  //rotate cube
+
+            for(int j = 6; j < 10;j++){
+                if(val[j] < 4){
+                    //bent
+                }else{
+                    //not bent
+                }
         }
-			} 
+			}
       catch (System.Exception) {}
 		}
 	}
